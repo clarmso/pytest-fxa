9586137e770a61f4df40e58e0c4a06a1d00688a4
Clare So <1740517+clarmso@users.noreply.github.com>
2026-03-23 10:43:08 -0400

    Beta 1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup
 
 setup(
-    version="1.7.0",
+    version="1.7.0b1",
     name="pytest-fxa-mte",
     description="pytest plugin for Firefox Accounts",
     long_description=open("README.rst").read(),
@@ -21,7 +21,7 @@ setup(
     license="Mozilla Public License 2.0 (MPL 2.0)",
     keywords="py.test pytest mozilla automation firefox account fxa",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Framework :: Pytest",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
@@ -32,10 +32,11 @@ setup(
         "Topic :: Software Development :: Testing",
         "Topic :: Utilities",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
